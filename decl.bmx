bmx-ng/bcc

commit fd1e552d5bf6482b684aa429838b509ac3265cc0

New() and Delete() cannot specify return types. Fixes #339.

decl.bmx
@@ -1965,9 +1965,13 @@ Type TFuncDecl Extends TBlockDecl
 		Local strictVoidToInt:Int = False
 
 		If isCtor() Or isDtor() Then
+			If retTypeExpr And Not TVoidType(retTypeExpr) Then
+				Err ident + "() cannot specify a return type"
+			End If
 			If ClassScope() And ClassScope().IsInterface() Then
 				Err ident + "() cannot be declared in an Interface."
 			End If
+			If IsCtor() retTypeExpr=New TObjectType.Create( TNewDecl(Self).cDecl )
 		End If
 
 		'semant ret type
@@ -2207,6 +2211,7 @@ End Type
 Type TNewDecl Extends TFuncDecl
 
 	Field chainedCtor:TNewExpr
+	Field cdecl:TClassDecl
 	
 	Method OnCopy:TDecl(deep:Int = True)
 		Local args:TArgDecl[]=argDecls[..]

ctranslator.bmx
@@ -4893,7 +4893,9 @@ End Rem
 			func :+ funcDecl.ident
 		End If
 
-		func :+ TransIfcType(funcDecl.retType, funcDecl.ModuleScope().IsSuperStrict())
+		If Not TNewDecl(funcDecl) Then
+			func :+ TransIfcType(funcDecl.retType, funcDecl.ModuleScope().IsSuperStrict())
+		End If
 
 		' function args
 		func :+ TransIfcArgs(funcDecl)

iparser.bmx
@@ -691,7 +691,7 @@ Type TIParser
 				'_toker.
 				NextToke
 				
-				Local decl:TFuncDecl = ParseFuncDecl( _toke,method_attrs|FUNC_METHOD )
+				Local decl:TFuncDecl = ParseFuncDecl( _toke,method_attrs|FUNC_METHOD, ,classDecl )
 				'If decl.IsCtor() decl.retTypeExpr=New TObjectType.Create( classDecl )
 				classDecl.InsertDecl decl
 				
@@ -862,7 +862,7 @@ Type TIParser
 		Return str
 	End Method
 
-	Method ParseFuncDecl:TFuncDecl( toke$,attrs:Int, returnType:TType = Null )
+	Method ParseFuncDecl:TFuncDecl( toke$,attrs:Int, returnType:TType = Null, classDecl:TClassDecl = Null )
 		SetErr
 
 		'If toke Parse toke
@@ -881,7 +881,7 @@ Type TIParser
 					NextToke
 					attrs:|FUNC_CTOR
 					attrs:&~FUNC_METHOD
-					ty=ParseDeclType(attrs, True)
+					ParseDeclType(attrs, True)
 				Else
 					If _toker._tokeType = TOKE_STRINGLIT Then
 						id = ParseStringLit()
@@ -1018,6 +1018,7 @@ Type TIParser
 		Local funcDecl:TFuncDecl
 		If attrs & FUNC_CTOR Then
 			funcDecl = New TNewDecl.CreateF( id,ty,args,attrs )
+			TNewDecl(funcDecl).cdecl = classDecl
 		Else
 			If fdecl Then
 				funcDecl = fdecl

parser.bmx
@@ -2795,6 +2795,7 @@ End Rem
 		Local funcDecl:TFuncDecl
 		If attrs & FUNC_CTOR Then
 			funcDecl=New TNewDecl.CreateF( id,ty,args,attrs )
+			TNewDecl(funcDecl).cdecl = classdecl
 		Else
 			'If fdecl Then
 			'	funcDecl = fdecl
@@ -3267,7 +3268,6 @@ End Rem
 					Err "Structs can only contain fields."
 				EndIf
 				Local decl:TFuncDecl=ParseFuncDecl( _toke,method_attrs | abst_attrs,classDecl )
-				If decl.IsCtor() decl.retTypeExpr=New TObjectType.Create( classDecl )
 				classDecl.InsertDecl decl
 			Case "function"
 				'If (attrs & CLASS_INTERFACE)

commit ca79b222371f121a50b480526c46f772eff9bb1c

Fixed erroneous "New() cannot specify a return type" compile error fd1e552d5bf6482b684aa429838b509ac3265cc0 broke TNewDecl.Copy by adding to TFuncDecl.OnSemant 1) a check that throws an error if the constructor already has a return type and 2) a statement which sets a return type afterwards a74c5236e6823a8fac615b2f9f2b2b62dafe81a0 makes use of TNewDecl.OnCopy merging both commits caused constructors to stop working

decl.bmx
@@ -2258,7 +2258,13 @@ Type TNewDecl Extends TFuncDecl
 		For Local i:Int=0 Until args.Length
 			args[i]=TArgDecl( args[i].Copy() )
 		Next
-		Local t:TNewDecl = TNewDecl(New TNewDecl.CreateF( ident,retType,args,attrs &~DECL_SEMANTED ))
+		Local retTypeCopy:TType
+		If IsSemanted() Then
+			retTypeCopy = Null
+		Else
+			retTypeCopy = retType
+		End If
+		Local t:TNewDecl = TNewDecl(New TNewDecl.CreateF( ident,retTypeCopy,args,attrs &~DECL_SEMANTED ))
 		If deep Then
 			For Local stmt:TStmt=EachIn stmts
 				t.AddStmt stmt.Copy(t)